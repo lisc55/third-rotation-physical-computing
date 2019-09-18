ad77d14d9a796657a8547ed0e07fdddbbeb866e9
JiayinHu <jiayin_hu@hotmail.com>
2019-09-18 14:34:49 -0400

    fixing win compile

--- scripts/run_assignment.bat
+++ scripts/run_assignment.bat
@@ -11,7 +11,7 @@ if "%test%"=="" set test=1
 cmake --build ./build --config Release --target %assignment%
 if not %ERRORLEVEL% == 0 goto :error
 
-build\proj\%assignment%\Release\%assignment%.exe -o build\proj\%assignment%\%test%\output -test %test%
+build\proj\%assignment%\Release\%assignment%.exe  -test %test%
 if not %ERRORLEVEL% == 0 goto :error
 
 goto :endofscript
